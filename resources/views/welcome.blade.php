9562c252e5d9930610fea412de70406b290a6d13
Ichchhitmd <hunterichchhit@live.com>
2021-04-07 09:53:37 +0545

    First Commit 2

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+@section('food')
+
+
+<div class="flex-center position-ref full-height">
+<p>
+<a class="btn btn-outline-secondary" href="/products">Our Specials!</a>
+</p>
+            <img src="/img/foodhublogo.png" alt="foodhub logo" width="500" height="340" position: bottom-right>
+        </div>
+    </div>
+
+</div>
+
+@endsection
